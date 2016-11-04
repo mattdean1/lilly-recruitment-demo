d7399c0c8e593e9b6d8b2b0027e9605b07fdf430
Matt Dean <matt.dean.001@gmail.com>
2016-11-04 00:11:00 +0000

    nodemon and display list

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var todolist = [{title: "todo1", description: "desc1"}, {title: "todo2", description: "desc2"}, {title: "todo3", description: "desc3"}];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { list: todolist });
 });
 
 module.exports = router;
