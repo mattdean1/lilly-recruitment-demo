81060f3fe67efaa622ec2766ced06d1c8cb9de37
Matt Dean <matt.dean.001@gmail.com>
2016-11-04 10:09:40 +0000

    add db

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-var todolist = [{title: "todo1", description: "desc1"}, {title: "todo2", description: "desc2"}, {title: "todo3", description: "desc3"}];
+var pg = require('pg');
+pg.defaults.ssl = true;
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { list: todolist });
+  var results = [];
+     pg.connect(process.env.DATABASE_URL, function(err, client) {
+       if (err) throw err;
+       client
+         .query('SELECT * FROM todo')
+         .on('row', function(row) {
+           results.push(row);
+         })
+         .on('end', function(){
+           res.render('index', { list: results });
+         });
+     });
 });
 
-//post /insert
+//insert new todo into database
 router.post('/insert', function(req, res) {
-  todolist.push({title: req.body.title, description: req.body.description});
-  res.render('index', { list: todolist });
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+     if (err) throw err;
+     client
+       .query('INSERT INTO todo (title, description) VALUES ($1, $2)', [req.body.title, req.body.description])
+       .on('end', function(){
+         res.send('success');
+       });
+   });
 });
 
 module.exports = router;
